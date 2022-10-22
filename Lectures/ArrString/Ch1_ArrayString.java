d4018c9bfc5b590203b08cc57b6f7cebb6ada99d
4562sky <4562sky@naver.com>
2022-10-22 14:43:25 +0900

    List를 배열로 정렬하기

--- Lectures/ArrString/Ch1_ArrayString.java
+++ Lectures/ArrString/Ch1_ArrayString.java
@@ -3,15 +3,9 @@ package Collection;
 import java.util.*;
 
 public class Ch1_ArrayString {
-
+	// Arraylist -> 배열 -> 정렬 -> 배열 -> ArrayList출력 하기
 	// string 정렬, binary search 구현
 	// 1단계: string, 2단계: string 객체, Person 객체들의 list\
-
-	public static String[] removeElement1(String[] arr, String item) {
-		List<String> list = new ArrayList<>(Arrays.asList(arr));
-		list.remove(item);
-		return list.toArray(String[]::new);
-	}
 
 	public static void main(String[] args) {
 
@@ -22,53 +16,37 @@ public class Ch1_ArrayString {
 		list1.add("서울");
 		list1.add("도쿄");
 		list1.add("뉴욕");
+		list1.add("도쿄");
+		list1.add("도쿄");
+		list1.add("도쿄");
+		list1.add("도쿄");
 
-		ArrayList<String> list2 = new ArrayList<String>();
-		list2.add("런던");
-		list2.add("로마");
-		list2.add("방콕");
-		list2.add("북경");
-		list2.add("도쿄");
-		list2.add("서울");
-		list2.add(1, "LA");
-
+		// ArrayList 상태에서의 정렬
 		// collection.sort하면 순서가 정렬된다
 		// System.out.println("collection.sort()::");
 		// Collections.sort(list1);
-		// System.out.println("list1::");
-		// for (String city : list1)
-		// System.out.print(city + " ");
-		// System.out.println();
-		// System.out.println("list2::");
-		// Collections.sort(list2);
-		// for (String city : list2)
-		// System.out.print(city + " ");
-
-		// Arraylist -> 배열 -> 정렬 -> 배열 -> ArrayList출력 하기
-
-		// Arraylist를 합침
-		List<String> list3 = new ArrayList<>();
-		list3.addAll(list1);
-		list3.addAll(list2);
 
 		// Arraylist를 배열로 만듦. 배열 이름은 cities
 		String cities[] = new String[0];
-		cities = list3.toArray(cities);
+		cities = list1.toArray(cities);
 
 		// cities 배열을 -> 정렬로 만들기
-		// collection.sort도 있지만 다른 방법으로 정렬
+		// collection.sort(ArrayList상태)도 있지만 다른 방법으로 정렬
 		// Arrays.sort도 있지만 다른 방법으로 정렬
+		
+		//배열의 정렬
 		String temp;
-		for (int i = 0; i < cities.length; i++) {
-			for (int j = i + 1; j < cities.length; j++) {
-				if (cities[i].compareTo(cities[j]) > 1) {
+		for(int i = 0; i < cities.length; i++ ) {
+			for (int j = i+1 ; j< cities.length; j++) {
+				if( cities[i].compareTo(cities[j]) >0 ) {
 					temp = cities[i];
 					cities[i] = cities[j];
-					cities[j] = temp;
+					cities[j] = temp; 
 				}
 			}
 		}
-		// Arrays.sort(cities);
+		
+		// cities 배열 출력
 		System.out.println("");
 		System.out.println("정렬후::");
 		for (String city : cities)
@@ -77,13 +55,16 @@ public class Ch1_ArrayString {
 		// 배열 -> 리스트
 		ArrayList<String> lst = new ArrayList<String>(Arrays.asList(cities));
 
-		// 중복제거
+		// 리스트로 만들어서 중복제거, 리스트는 수동으로 크기를 줄여줘야함.
 		int count = cities.length;
 		for (int i = 0; i < count; i++) {
-			for (int j = i + 1; j < count; j++) {
-				if ((lst.get(i))==(lst.get(j))) {
-					lst.remove(j); count--;
-				}
+			int j = i + 1;
+			while (j < count) {
+				if ((lst.get(i)) == (lst.get(j))) {
+					lst.remove(j);
+					count--;
+				} else
+					j++;
 			}
 		}
 		System.out.println("");
