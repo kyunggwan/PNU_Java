f3d0620b8f7bc0cf8425ceb40cbe7c1dbc1b94d5
4562sky <4562sky@naver.com>
2022-11-21 23:05:15 +0900

    class, non-static으로 구현

--- SelfStudy/혼자 문제풀기/2진수 출력/Binary3.java
+++ SelfStudy/혼자 문제풀기/2진수 출력/Binary3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 //값을 입력받아 2진수 출력
-//class, non-static 메소드 활용
+//class, non-static 활용
 class Cal{
     public void calculate(int num){
         //배열 arr에 담았다가 역순으로 출력할 예정
@@ -8,16 +8,14 @@ class Cal{
         //배열 초기값 지정
         int i = 0;
 
+        //2진수 구현부분
         while (num > 0) {
-            //2로 나눈 나머지를 배열에 넣는다.
             arr[i] = num % 2;
-            //그리고 입력값을 2로 나눠줌
             num /= 2;
-            //다음 index에 넣어야 하므로 i 값 증가
             i++;
         }
-        //while문을 벗어난 경우는 num이 0이므로 증가한 i값을 감소시켜 줘야함
         i--;
+
         //넣어준 배열의 역순으로 출력
         for(; i >=0; i--){
             System.out.print(arr[i]);
@@ -26,14 +24,13 @@ class Cal{
 }
 
 public class Binary3 {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         //num은 입력 받은 수
         int num = sc.nextInt();
 
+        //class, non-static 출력
         Cal c = new Cal();
-        //calculate 메소드 호출
         c.calculate(num);
 
     }
