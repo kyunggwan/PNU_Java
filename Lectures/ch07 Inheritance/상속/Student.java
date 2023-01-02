a00ff4a36a5006533205aa412d6acdd70c2a8dee
4562sky <4562sky@naver.com>
2023-01-02 22:12:39 +0900

    상속

---
+++ Lectures/ch07 Inheritance/상속/Student.java
@@ -0,0 +1,12 @@
+public class Student extends Person{
+
+        private String major;
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+}
