cf568d24f2816c7a17230472761be3475e3049ec
4562sky <4562sky@naver.com>
2023-01-09 22:55:08 +0900

    단순 출력

---
+++ SelfStudy/Baekjoon 문제풀기/step1_inoutput/q14_sprout_25083.java
@@ -0,0 +1,12 @@
+package BJStep1_Inoutput;
+
+public class q14_sprout_25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
