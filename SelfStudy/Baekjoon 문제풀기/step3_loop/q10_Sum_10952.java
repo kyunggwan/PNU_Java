6c4d8ecc5d0403c08dcdc01e68a29bf399b7b107
4562sky <4562sky@naver.com>
2023-01-11 19:40:24 +0900

    Buffered while문 이용

---
+++ SelfStudy/Baekjoon 문제풀기/step3_loop/q10_Sum_10952.java
@@ -0,0 +1,26 @@
+package BJStep3_Loop;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class q10_Sum_10952 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        while(true) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            if(A == 0 && B == 0){
+                break;
+            }
+            sb.append((A+B)).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
