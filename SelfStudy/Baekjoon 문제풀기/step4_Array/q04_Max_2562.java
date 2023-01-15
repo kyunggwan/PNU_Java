3f602899abaf2fb68faa207ecc3f81bc93949087
kyunggwan <4562sky@naver.com>
2023-01-15 21:30:00 +0900

    백준 2562

---
+++ SelfStudy/Baekjoon 문제풀기/step4_Array/q04_Max_2562.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class q04_Max_2562 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[9];
+        int max = 0;
+        int index = 0;
+
+        for(int i = 0; i < 9; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < 9; i++){
+            if(arr[i] > max){
+                max = arr[i];
+                index = i+1;
+            }
+        }
+        System.out.println(max);
+        System.out.println(index);
+    }
+}
