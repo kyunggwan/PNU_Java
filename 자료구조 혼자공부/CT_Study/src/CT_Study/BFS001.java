54a564510af82442f61702564071060ce53c0f9a
kyunggwan <4562sky@naver.com>
2023-05-09 10:26:45 +0900

    "feat: BFS 시작, 이론"

---
+++ 자료구조 혼자공부/CT_Study/src/CT_Study/BFS001.java
@@ -0,0 +1,19 @@
+package CT_Study;
+
+public class BFS001 {
+    // 너비 우선 탐색
+    // 시작노드 0을 Queue에 넣는다.
+    // 0을 꺼내고, child(1)을 추가한다. 꺼낸 0은 출력한다.
+    // Queue에서 1을 꺼내고, child(2,3)을 추가한다. 꺼낸 1은 출력한다.
+    // Queue에서 2를 꺼내고, child(4)를 추가한다. 꺼낸 2는 출력한다.
+    // Queue에서 3를 꺼내고, child(5)를 추가한다. 꺼낸 3은 출력한다.
+    // Queue에서 4를 꺼내고, child가 없으므로 꺼낸 4만 출력한다.
+    // Queue에서 5를 꺼내고, child(6,7)을 추가한다. 꺼낸 5는 출력한다.
+    // Queue에서 6을 꺼내고, child(8)을 추가한다. 꺼낸 6은 출력한다.
+    // Queue에서 7을 꺼내고, child가 없으므로 꺼낸 7만 출력한다.
+    // Queue에서 8을 꺼내고, child가 없으므로 꺼낸 8만 출력한다.
+    // Queue가 비면 출력
+    public static void main(String[] args) {
+
+    }
+}
