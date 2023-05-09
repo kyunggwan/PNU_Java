5558610467b1d21d3aa06a57d2fafea2f8806ee0
kyunggwan <4562sky@naver.com>
2023-05-09 10:26:52 +0900

    "feat: DFS 시작, 이론"

---
+++ 자료구조 혼자공부/CT_Study/src/CT_Study/DFS001.java
@@ -0,0 +1,16 @@
+package CT_Study;
+
+public class DFS001 {
+    // 깊이 우선 탐색
+    // 0을 넣음
+    // 0을 꺼내고 child들(1)을 stack에 담는다. 꺼낸 0은 출력한다
+    // 1을 꺼내고 child들(2,3)을 stack에 담는다. 꺼낸 1은 출력한다.
+    // 3을 꺼내고 child들(4,5)을 stack에 담는다. 꺼낸 3은 출력한다.
+    // 5를 꺼내고 child들(6,7)을 stack에 담는다. 꺼낸 5는 출력한다.
+    // 7을 꺼내고 child가 없어서 7을 출력한다.
+    // 6을 꺼내고 child(8)을 stack에 담는다. 꺼낸 6은 출력한다.
+    // stack에서 데이터를 꺼내고, child가 없으면 출력한다. 반복
+    public static void main(String[] args) {
+
+    }
+}
