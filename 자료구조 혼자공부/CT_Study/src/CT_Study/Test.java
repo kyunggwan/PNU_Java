f6a6422e0890a7b8b73da1cfa32403fde9e4cac6
kyunggwan <4562sky@naver.com>
2023-05-09 10:26:28 +0900

    "feat: Queue 테스트"

---
+++ 자료구조 혼자공부/CT_Study/src/CT_Study/Test.java
@@ -0,0 +1,21 @@
+package CT_Study;
+import java.util.NoSuchElementException;
+import CT_Study.Queue;
+
+public class Test {
+    public static void main(String[] args) {
+        Queue<Integer> q = new Queue<Integer>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.peek());
+        System.out.println(q.remove());
+        System.out.println(q.isEmpty());
+        System.out.println(q.remove());
+        System.out.println(q.isEmpty());
+
+    }
+}
